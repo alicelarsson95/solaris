92c29656957921c4be97d7d31254992f2c1b3d63
alicelarsson95 <alicelarsson95@hotmail.com>
2024-11-28 11:34:34 +0100

    hejsan

---
+++ script.js
@@ -0,0 +1,213 @@
+const getApiKey = async () => {
+  const url = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys";
+
+  const options = {
+    method: "POST",
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const apiData = await response.json();
+
+    console.log("This is your API-key:", apiData.key);
+
+    return apiData.key
+  } catch (error) {
+    console.error("Error fetching the API-key:", error);
+  }
+};
+const getPlanet = async (key) => {
+  const url = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "x-zocom": key,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const planetData = await response.json();
+
+    console.log("Planets:", planetData);
+
+    return planetData;
+  } catch (error) {
+    console.error("Error catching the planets:", error);
+  }
+};
+
+const sun = document.querySelector(".planet-sun");
+sun.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[0])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const mercury = document.querySelector(".planet-1")
+mercury.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[1])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const venus = document.querySelector(".planet-2")
+venus.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[2])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const earth = document.querySelector(".planet-3")
+earth.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[3])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const mars = document.querySelector(".planet-4")
+mars.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[4])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const jupiter = document.querySelector(".planet-5")
+jupiter.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[5])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const saturn = document.querySelector(".planet-6")
+saturn.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[6])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const uranus = document.querySelector(".planet-7")
+uranus.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[7])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+const neptunus = document.querySelector(".planet-8")
+neptunus.addEventListener('click', async () => {
+  try {
+    const key = await getApiKey()
+    const apiData = await getPlanet(key);
+
+    console.log(apiData);
+
+    displayInfo(apiData.bodies[8])
+  
+  } catch (error) {
+    console.error('Error occuored:', error);
+    
+  }
+  startSite.style.display = 'none'
+  planetSite.style.display = 'flex'
+})
+
+
+const startSite = document.querySelector('.site-wrapper')
+const planetSite = document.querySelector('.site-wrapper2')
+const planetHeader = document.querySelector('.header.header2')
+const subHeader = document.querySelector('')
+
+const displayInfo = (infoBodies) => {
+
+  planetHeader.innerText = infoBodies.name
+  console.log(infoBodies);
+}
